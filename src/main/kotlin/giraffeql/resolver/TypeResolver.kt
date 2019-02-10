1bd11c776ec082eed88890f4cf08d3fa0902c1e8
Matt Stewart <smtp.matt@gmail.com>
2019-02-10 15:35:22 +1100

    master cleanup type resolvers

--- src/main/kotlin/giraffeql/resolver/TypeResolver.kt
+++ src/main/kotlin/giraffeql/resolver/TypeResolver.kt
@@ -8,9 +8,9 @@ import kotlin.reflect.KProperty1
 import kotlin.reflect.KType
 
 interface TypeResolver {
-//    fun resolve(type: KType, env: TypeResolverEnvironment): GraphQLType? = resolve(type.toKClass(), env)?.wrapNonNull(type)
-    fun resolve(property: KProperty1<*, *>, env: TypeResolverEnvironment): GraphQLType? =
-        resolve(property.returnType.toKClass(), env)?.wrapNonNull(property.returnType)
+    fun resolve(type: KType, env: TypeResolverEnvironment): GraphQLType? = resolve(type.toKClass(), env)?.wrapNonNull(type)
+//    fun resolve(property: KProperty1<*, *>, env: TypeResolverEnvironment): GraphQLType? =
+//        resolve(property.returnType.toKClass(), env)?.wrapNonNull(property.returnType)
 
     fun resolve(kClass: KClass<*>, env: TypeResolverEnvironment): GraphQLType? = null
 }
